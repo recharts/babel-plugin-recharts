ab479fd26330b31d0e4f235d762ce361b0e052e1
jasonHzq <hzongquan@126.com>
2016-05-18 20:57:44 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# babel-plugin-recharts
+
+A babel plugin help you import less Recharts modules.
+
+[![npm version](https://badge.fury.io/js/babel-plugin-recharts.png)](https://badge.fury.io/js/babel-plugin-recharts)
+[![npm downloads](https://img.shields.io/npm/dt/babel-plugin-recharts.svg?style=flat-square)](https://www.npmjs.com/package/babel-plugin-recharts)
+
+
+## install
+```
+$ npm i -D babel-plugin-recharts
+```
+
+## Example
+
+from
+
+```
+import { Line, Pie, Treemap } from 'recharts';
+```
+
+to
+
+```
+import Treemap from 'recharts/chart/Treemap';
+import Pie from 'recharts/polar/Pie';
+import Line from 'recharts/cartesian/Line';
+```
+
+## Usage
+
+### .babelrc
+
+```js
+{
+  "plugins": ["recharts"],
+  "presets": ["es2015"]
+}
+```
+
+
+### webpack.config.js
+
+```
+'module': {
+  'loaders': [{
+    'loader': 'babel-loader',
+    'test': /\.js$/,
+    'exclude': /node_modules/,
+    'query': {
+      'plugins': ['recharts'],
+      'presets': ['es2015']
+    }
+  }]
+}
+```
+
+## Limitations
+
+* You must use ES2015 imports to load recharts
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2015-2016 Recharts Group
---
+++ src/index.js
@@ -0,0 +1,67 @@
+import Module from 'module';
+import path from 'path';
+import fs from 'fs';
+
+const recharts = 'recharts';
+const rechartsLib = 'recharts/lib';
+
+const _module = new Module();
+
+const rechartsPath = path.dirname(Module._resolveFilename('recharts', {
+  ..._module,
+  paths: Module._nodeModulePaths(process.cwd()),
+}));
+
+const indexFile = fs.readFileSync(rechartsPath + '/../src/index.js', 'utf-8');
+const regStr = 'export ([\\S]+) from \'([\\S]+)\';';
+
+const regx = new RegExp(regStr);
+const globalRegx = new RegExp(regStr, 'g');
+
+const pkgMap = indexFile.match(globalRegx)
+  .map(exp => exp.match(regx).slice(1))
+  .reduce((result, [pkgId, path]) => {
+    return {
+      ...result,
+      [pkgId]: path.slice(1),
+    };
+  }, {});
+
+export default function ({types: t}) {
+  return {
+    visitor: {
+      ImportDeclaration(path) {
+        const { node } = path;
+        const { specifiers, source } = node;
+        const { value: pkgId } = source;
+        const specs = [];
+
+        if (pkgId !== recharts) {
+          return ;
+        }
+
+        if (!specifiers.filter(t.isImportSpecifier).length) {
+          return;
+        } 
+
+        specifiers.forEach(spec => {
+          const { local , imported } = spec;
+          const { name: localName } = local;
+
+          let importedPath = recharts;
+
+          if (t.isImportSpecifier(spec)) {
+            const { name: importedName } = imported;
+
+            spec = t.importDefaultSpecifier(t.identifier(localName));
+            importedPath += pkgMap[importedName];
+          }
+
+          path.insertAfter(t.importDeclaration([spec], t.stringLiteral(importedPath)));
+        });
+
+        path.remove();
+      }
+    }
+  };
+}
